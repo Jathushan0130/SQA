9184511cea4371bdf2484c9185372d1510fa185e
wikto <victor.ryzak@ontariotechu.net>
2025-02-04 14:36:43 -0500

    defined functions

--- Main.java
+++ Main.java
@@ -2,6 +2,39 @@ public class Main
 {
 public static void main(String[] args)
 {
+    //login and logout be done in main
+
+}
+public static void withdrawl()
+{
+
+}
+public static void deposit()
+{
+
+}
+public static void create()
+{
+
+}
+public static void changePlan()
+{
+
+}
+public static void delete()
+{
+
+}
+public static void disable()
+{
+
+}
+public static void transfer()
+{
+
+}
+public static void paybill()
+{
 
 }
 }
