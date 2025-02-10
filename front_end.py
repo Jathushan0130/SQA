22d28f12473f230aedc83774e2e9e9e12215d7c5
wikto <victor.ryzak@ontariotechu.net>
2025-02-10 13:01:24 -0500

    added functions

---
+++ front_end.py
@@ -0,0 +1,32 @@
+def login():
+    DEFW 
+
+def withdrawl():
+    wqd
+
+def Deposit():
+    qwde
+
+def create():
+    qd
+
+def changePlan():
+    dwe
+
+def delete():
+    Dwefd
+
+def disable():
+    Un
+
+def logout():
+    efdwefr
+
+def Transfer():
+    dwewf
+
+def payBill():
+    dwefef
+
+def console():
+    dwf
