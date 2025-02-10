9e4c51c9893b7223033a8b066ffc956574fd666d
wikto <victor.ryzak@ontariotechu.net>
2025-02-10 12:33:11 -0500

    added file creation/ check if file exists

--- Main.java
+++ Main.java
@@ -1,8 +1,24 @@
+import java.io.File;  // Import the File class
+import java.io.FileWriter;   // Import the FileWriter class
+import java.io.IOException;  // Import the IOException class to handle errors
+
 public class Main
 {
 public static void main(String[] args)
 {
     //login and logout be done in main
+    try{
+        File myObj = new File("Bank transaction file");
+        if (myObj.createNewFile()) {
+        System.out.println("File created: " + myObj.getName());
+      } else {
+        System.out.println("File already exists.");
+    }
+    }catch(IOException e){
+        System.out.println("An error occurred.");
+       e.printStackTrace();
+
+    }
 
 }
 public static void withdrawl()
